0516112891d8d7c5913d8906e168acae297bd3e3
kwanung.park <kwanung.park@linecorp.com>
2024-01-24 15:18:21 +0900

    update babel config

--- babel.config.js
+++ babel.config.js
@@ -17,7 +17,12 @@ module.exports = (api) => {
         }
       ],
       '@babel/preset-typescript',
-      '@babel/preset-react'
+      [
+        '@babel/preset-react',
+        {
+          runtime: 'automatic'
+        }
+      ]
     ],
     plugins: [
       ...[
